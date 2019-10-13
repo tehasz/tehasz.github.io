cd5bfe1f563aa6b842577c51ba0b225c7ecc9043
tehasz <51036396+tehasz@users.noreply.github.com>
2019-10-13 10:48:57 -0400

    final changes to unit 1

--- Unit1/js/invitation.js
+++ Unit1/js/invitation.js
@@ -2,13 +2,15 @@
 function replaceContent() {
 	//declaring the variables
 	var myRecipientName;
-	var HostName
+	var HostName;
 	
 	//setting the variable to the input field's id named recipientNameInput's value
 	myRecipientName = document.getElementById("recipientNameInput").value;
 	HostName = document.getElementById("hostNameInput").value;
-	
+
+	// log storage of variables show process in console
 	console.log('Variable myRecipientName: ' + myRecipientName);
+	console.log('Variable HostName: ' + HostName);
 	
 	//setting the HTML code in the span id recipientNamePlaceholder with the variable 
 	document.getElementById("recipientNamePlaceholder").innerHTML = myRecipientName;
