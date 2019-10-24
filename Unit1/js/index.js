0b0ae93183d62422f826cd2b64299bfc3ff40365
tehasz <51036396+tehasz@users.noreply.github.com>
2019-10-24 10:59:47 -0400

    edit

--- Unit1/js/index.js
+++ Unit1/js/index.js
@@ -3,15 +3,27 @@ window.onload = rotate;
 
 // variable declaration
 var theCount = 0;
-var exBanner = new Array("images/banner1.jpg", "images/banner2.jpg","images/banner3.jpg");
+
+var banner1 = newImage();
+var banner2 = newImage();
+var banner3 = newImage();
+
+banner1.src = '/images/banner1.jpg';
+banner2.src = '/images/banner2.jpg';
+banner3.src = '/images/banner3.jpg';
+
+var banner = [];
+banner.push(banner1);
+banner.push(banner2);
+banner.push(banner3);
 
 // function to rotate banner images
 function rotate() {
     theCount++;
-    if (theCount == exBanner.length){
+    if (theCount === banner.length){
         theCount = 0;
     }
-    document.getElementById("exBanner").src = exBanner[theCount];
+    document.getElementById("exBanner").src = banner[theCount];
 
     setTimeout(rotate, 3 * 1000);
 }
